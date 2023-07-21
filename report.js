8a62873b01a19987e46ac1d2f8c5919642a5b5eb
Moha2653 <m.saidalmasri@hotmail.com>
2023-07-20 23:04:04 -0400

    csv and txt report file savers

--- report.js
+++ report.js
@@ -1,17 +1,35 @@
+const {convertArrayToCSV} = require('convert-array-to-csv')
+const fs = require('fs')
+
 function printReport(pages){
   console.log("===========");
-  console.log("REPORT");
+  console.log("   REPORT");
   console.log("===========");
   const sortedPages = sortPages(pages)
+
+  console.log(sortedPages[0][0])
+  const links = []
+  const rights = []
   for (const sortedPage of sortedPages) {
     const url = sortedPage[0]
     const hits = sortedPage[1]
-    console.log(`Found ${hits} links to page: ${url}`);  
+    links.push({[url]:hits})
+    rights.push([url, hits])
+    console.log(`Found ${hits} links to page: ${url}`);
   }
-  console.log("===========");
+
+  const csvFromArr = convertArrayToCSV(rights, {
+    header:['Url Path', 'Num of links'],
+    separator:';'
+  }) 
+  fs.writeFileSync(`${sortedPages[0][0]}.txt`, JSON.stringify(links))
+  fs.writeFileSync(`${sortedPages[0][0]}.csv`, csvFromArr)
+
+  console.log("=============");
   console.log("END OF REPORT");
-  console.log("===========");
+  console.log("=============");
 }
+
 
 function sortPages(pages) {
   const pagesArr = Object.entries(pages)
@@ -20,6 +38,7 @@ function sortPages(pages) {
     bHits = b[1]
     return b[1] - a[1]
   })
+
   return pagesArr
 }
 
